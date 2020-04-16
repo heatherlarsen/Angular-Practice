387bc96d537fc64408a3344f07f62272047cc5b2
Heather Larsen <heatherlarsenux@gmail.com>
2020-04-16 09:48:08 -0500

    WIP // Refactoring to match class example

--- Lesson-2/APM-Start/src/app/products/product-detail.component.ts
+++ Lesson-2/APM-Start/src/app/products/product-detail.component.ts
@@ -10,35 +10,24 @@ import { ProductService } from './product.service';
 })
 export class ProductDetailComponent implements OnInit {
 	pageTitle: string = 'Product Detail';
-	products: IProduct[];
-  product: IProduct;
-  errorMessage: string;
+  product: IProduct | undefined;
+  errorMessage: string = '';
 
   	constructor(private route: ActivatedRoute, 
   				private router: Router, 
           private ProductService: ProductService) { }
 
   	ngOnInit() {
-  		let id = +this.route.snapshot.paramMap.get('id');
-  		//this.pageTitle += `: ${id}`;
-      this.ProductService.getProducts().subscribe({
-        next: products => {
-          // Get Specific Product
-          for (let product of products) {
-            if (product.productId === id) {
-              this.product = {
-                'productId': product.productId,
-                'productName': product.productName,
-                'productCode': product.productCode,
-                'releaseDate': product.releaseDate,
-                'price': product.price,
-                'description': product.description, 
-                'starRating': product.starRating,
-                'imageUrl': product.imageUrl
-              };
-            }
-          }
-        }, 
+  		const param = this.route.snapshot.paramMap.get('id');
+      if (param) {
+        const id = +param;
+        this.getProduct(id);
+      }
+    }
+
+    getProduct(id: number) {
+      this.ProductService.getProduct(id).subscribe({
+        next: product => this.product = product,
         error: err => this.errorMessage = err
       });
   	}
