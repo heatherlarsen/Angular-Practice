b642d827667ad1519150e8a57488f8c55ef9e795
Heather Larsen <heatherlarsenux@gmail.com>
2020-04-16 09:38:34 -0500

    WIP // Initial attempt at Product Details page

--- Lesson-2/APM-Start/src/app/products/product-detail.component.ts
+++ Lesson-2/APM-Start/src/app/products/product-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,24 +10,37 @@ import { IProduct } from './product';
 })
 export class ProductDetailComponent implements OnInit {
 	pageTitle: string = 'Product Detail';
-	product: IProduct;
+	products: IProduct[];
+  product: IProduct;
+  errorMessage: string;
 
   	constructor(private route: ActivatedRoute, 
-  				private router: Router) { }
+  				private router: Router, 
+          private ProductService: ProductService) { }
 
   	ngOnInit() {
   		let id = +this.route.snapshot.paramMap.get('id');
-  		this.pageTitle += `: ${id}`;
-  		this.product = {
-  			'productId': id,
-  			'productName': 'Leaf Rake',
-  			'productCode': 'GDN-0011',
-  			'releaseDate': 'March 19, 2019',
-  			'description': 'Leaf rake with 48-inch wooden handle.',
-  			'price': 19.95,
-  			'starRating': 3.2,
-  			'imageUrl': 'assets/images/leaf_rake.png'
-  		}
+  		//this.pageTitle += `: ${id}`;
+      this.ProductService.getProducts().subscribe({
+        next: products => {
+          // Get Specific Product
+          for (let product of products) {
+            if (product.productId === id) {
+              this.product = {
+                'productId': product.productId,
+                'productName': product.productName,
+                'productCode': product.productCode,
+                'releaseDate': product.releaseDate,
+                'price': product.price,
+                'description': product.description, 
+                'starRating': product.starRating,
+                'imageUrl': product.imageUrl
+              };
+            }
+          }
+        }, 
+        error: err => this.errorMessage = err
+      });
   	}
 
   	onBack(): void {
